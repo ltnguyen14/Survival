6bd574b89240501602730fd3c9e3036d6a55a214
Nguyen, Lam <lamdr96al@gmail.com>
2019-01-22 13:39:02 -0500

    Box Collider

---
+++ Survival/engine/components/physics/collision/BoxCollider.cpp
@@ -0,0 +1,6 @@
+#include "BoxCollider.h"
+
+void BoxCollider::HookEntity(const Entity* entity)
+{
+    m_focusedEntity = entity;
+}
--- Survival/engine/components/physics/collision/BoxCollider.h
+++ Survival/engine/components/physics/collision/BoxCollider.h
@@ -1,10 +1,18 @@
 #pragma once
 
-class BoxCollider {
+#include "../../../platform/math/Math.h"
+#include "../../../platform/entities/entity.h"
+
+class BoxCollider : public Entity {
 public:
-	BoxCollider();
+	BoxCollider(glm::vec2 size) : m_size(size), m_centerPoint(0, 0);
 	virtual ~BoxCollider();
 
-private:
+	void HookEntity(const Entity* entity);
+	void Update()
 
+private:
+	Entity* m_focusedEntity;
+	glm::vec2 m_centerPoint;
+	glm::vec2 m_size;
 };
